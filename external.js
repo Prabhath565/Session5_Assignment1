8186db12f56cfd980263fb48f6ccb6dc85b81606
Prabhath565 <prabhath565@gmail.com>
2016-12-14 22:19:02 +0530

    S5_1

---
+++ external.js
@@ -0,0 +1,9 @@
+var  name = "Smith";
+		var  age  = 29;
+var DOB="28/10/1993";
+var POB="Warangal";
+		alert("Name = " +name  + " , Age = "+age + ", Place Of Birth = "+ POB+ ", DOB = " +DOB );
+console.log(name);
+console.log(age);
+console.log(DOB);
+console.log(POB);
